5cace1f1f1fc3d77c68d51a447b3fdafbd6ca97d
miladiashe <dkdud0122@naver.com>
2021-03-13 18:09:05 +0900

    숙제중

---
+++ IdTest.java
@@ -0,0 +1,8 @@
+public class IdTest {
+    public static void main (String[] args){
+        String 이름 = "김아영";
+        String name = "Kim Ayoung";
+        System.out.println("안녕하세요, "+이름+"님" + "("+name+")");
+    }
+    
+}
